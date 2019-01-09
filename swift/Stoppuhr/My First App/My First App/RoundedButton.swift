c161d067481257a385ff2a053a167918171d3cdf
Reinhold Lehnhardt <reinhold.lehnhardt@wabsolute.de>
2019-01-09 17:35:07 +0100

    init repository commit

---
+++ swift/Stoppuhr/My First App/My First App/RoundedButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedButton.swift
+//  My First App
+//
+//  Created by Reinhold Lehnhardt-Niederstebruch on 02.10.18.
+//  Copyright © 2018 Reinhold Lehnhardt-Niederstebruch. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
